6a4fe88f79bf6fa44a22bbc56014568d2c33be2c
emmyvander <40799536+emmyvander@users.noreply.github.com>
2019-04-22 19:02:48 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+/* Hide Image - Question 3 */
+function hideit() {
+  document.getElementById("something").className = "hidden";
+}
+
+/* Change Text - Question 4 */
+function changeText() {
+  document.getElementById("p1").style.fontSize = "7em";
+  document.getElementById("p1").style.fontStyle = "italic";
+  document.getElementById("p1").style.backgroundColor = "blue";
+  document.getElementById("p1").style.fontWeight = "bold";
+}
+
+/* Refresh Page - Question 4 & 5 */
+function refresh() {
+  location.reload();
+}
+
+/* Alter Text - Question 5 */
+function alterText() {
+    document.getElementById("class").style.fontSize = "6em";
+    document.getElementById("class").style.fontStyle = "italic";
+    document.getElementById("class").style.backgroundColor = "gray";
+    document.getElementById("class").style.fontWeight = "bold";
+}
